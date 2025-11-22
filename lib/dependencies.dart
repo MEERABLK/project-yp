50b8e120b13f6bdf8a372414c48e997f2c7fe400
yangchengyu <158498806+yang-cheng-yu@users.noreply.github.com>
2025-11-21 20:31:27 -0500

    centralized dependencies

---
+++ lib/dependencies.dart
@@ -0,0 +1,4 @@
+export 'package:flutter/material.dart';
+export 'package:cloud_firestore/cloud_firestore.dart';
+export 'package:firebase_core/firebase_core.dart';
+export 'package:flutter_hex_color/flutter_hex_color.dart';
--- lib/loginView.dart
+++ lib/loginView.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_hex_color/flutter_hex_color.dart';
+import 'dependencies.dart';
 import 'pages.dart';
 
 class LoginScreen extends StatefulWidget {
