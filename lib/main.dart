649a957f5b8d28da0d9fc7ca87b43944c8a722f2
yangchengyu <158498806+yang-cheng-yu@users.noreply.github.com>
2025-11-21 20:31:27 -0500

    centralized dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_hex_color/flutter_hex_color.dart';
+import 'dependencies.dart';
 import 'pages.dart';
 
 void main()async {
